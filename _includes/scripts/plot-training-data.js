cba9a5f9d6ebc63b3f7953faa6025c539389ec23
Florian Mladitsch <florian.mladitsch@adcubum.com>
2019-08-27 22:04:17 +0200

    minor cleanup

--- _includes/scripts/plot-training-data.js
+++ _includes/scripts/plot-training-data.js
@@ -1,12 +1,7 @@
 (function () {
     const plot = new Plot('graphTrainingData', 5, 10000, 300, 300000,
         'qm^2', 'EUR');
-    trainingData.forEach((data, index) => {
+    trainingData.forEach((data) => {
         plot.drawPoint(data);
     });
-    /*
-    plot.drawFunction((x) => {
-        return 1000*x + 200;
-    });
-     */
 })();
